2386845438cebbe9219ea71bfe7cb8b959d0b803
Asad Baig <36822413+AsadBaig039@users.noreply.github.com>
2021-09-05 15:48:20 +0500

    API Integration

---
+++ src/res/constants/cities.js
@@ -0,0 +1,31 @@
+export const Cities = [
+  {
+    label: 'Karachi',
+    value: {
+      city: 'Karachi',
+      lat: '24.8600',
+      lng: '67.0100',
+    },
+    country: 'Pakistan',
+    iso2: 'PK',
+    admin_name: 'Sindh',
+    capital: 'admin',
+    population: '14835000',
+    population_proper: '14835000',
+  },
+  {
+    label: 'Lahore',
+    value: {
+      city: 'Lahore',
+      lat: '31.5497',
+      lng: '74.3436',
+      key: 'asdfghjkjhgfdsa1',
+    },
+    country: 'Pakistan',
+    iso2: 'PK',
+    admin_name: 'Punjab',
+    capital: 'admin',
+    population: '11021000',
+    population_proper: '11021000',
+  },
+];
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {Cities} from '../res/constants/cities';
+import DropDownPicker from 'react-native-dropdown-picker';
+import {WEATHER_API_KEY} from '../utils/weatherKey';
+
+const API_KEY = WEATHER_API_KEY;
+console.log(API_KEY);
 
 const HomeScreen = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState({});
+  const [items, setItems] = useState(Cities);
+
+  // weather api call states
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${value.lat}&lon=${
+        value.lng
+      }&exclude=${'hourly,minutely'}&appid=${API_KEY}`,
+    )
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error))
+      .finally(() => setLoading(false));
+  }, [value]);
+
+  console.log('value', value);
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Select City</Text>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        onChangeValue={value => {
+          setValue(value);
+        }}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default HomeScreen;
